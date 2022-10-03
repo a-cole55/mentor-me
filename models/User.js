a0d6c7d11bafbbf79c98a9dbd7fe55931fa9eb80
a-cole55 <arniqua_c@yahoo.com>
2022-10-03 11:01:28 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,99 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userName: { 
+    type: String, 
+    unique: false,
+    require: true,
+    min: 3,
+  },
+  email: { 
+    type: String, 
+    unique: true,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    min: 6
+},
+title: {
+  type: String,
+  required: true,
+},
+companySchool:{
+  type: String,
+  default: ""
+},
+image: {
+  type: String,
+  require: true,
+},
+cloudinaryId: {
+  type: String,
+  require: true,
+},
+  favorites:{
+    type: Array,
+    default: [],
+  },
+  desc:{
+    type: String,
+    default: "",
+  },
+  city:{
+    type: String,
+    max:50
+  },
+  facebook:{
+    type: String,
+    default: ""
+  },
+  instagram:{
+    type: String,
+    default: ""
+  },
+  twitter:{
+    type: String,
+    default: ""
+  },
+  linkedIn:{
+    type: String,
+    default: ""
+  },
+});
+
+// Password hash middleware.
+
+UserSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Helper method for validating user's password.
+
+UserSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
